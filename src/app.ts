dd479ca68d03af4403e226a5d5bfb2eae995d29e
mohid <mohid10587@gmail.com>
2023-07-30 08:27:44 +0600

    make change in ts confige file

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use('/api/v1/users/', UserRoutes);
-// app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
 app.use('/api/v1', routes);
 
 //global error handler
